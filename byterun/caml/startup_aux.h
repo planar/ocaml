c9c5f1273bdc1919531407e5180ed30bfabd2e44
Damien Doligez <damien.doligez@inria.fr>
2018-04-20 14:55:23 +0200

    add caml_alloc_custom_mem and corresponding GC parameters

--- byterun/caml/startup_aux.h
+++ byterun/caml/startup_aux.h
@@ -29,6 +29,9 @@ extern uintnat caml_init_heap_chunk_sz;
 extern uintnat caml_init_heap_wsz;
 extern uintnat caml_init_max_stack_wsz;
 extern uintnat caml_init_major_window;
+extern uintnat caml_init_custom_major_ratio;
+extern uintnat caml_init_custom_minor_ratio;
+extern uintnat caml_init_custom_minor_max_bsz;
 extern uintnat caml_trace_level;
 extern uintnat caml_cleanup_on_exit;
 
