2a61e96dde1557116bf864fca00f0a309b759b89
Damien Doligez <damien.doligez@inria.fr>
2021-03-25 13:29:50 +0100

    better version of `path_polls` and `requires_prologue_polls`

--- asmcomp/polling.ml
+++ asmcomp/polling.ml
@@ -16,58 +16,93 @@ open Mach
 
 module String = Misc.Stdlib.String
 
-(* Check a sequence of instructions from [f] and return whether
-   they poll (via an alloc or raising an exception) *)
-let rec path_polls (f : Mach.instruction) : bool =
-  match f.desc with
-  | Iifthenelse (_, i0, i1) ->
-      ((path_polls i0) && (path_polls i1)) || (path_polls f.next)
-  | Iswitch (_, acts) ->
-      (Array.for_all path_polls acts) && (path_polls f.next)
-  | Icatch (_, handlers, body) ->
-     (path_polls f.next) && (path_polls body)
-     &&
-     (List.for_all path_polls (List.map (fun (_, handler_code) ->
-      handler_code) handlers))
-  | Itrywith (body, handler) ->
-      (path_polls body) && (path_polls handler) && (path_polls f.next)
-  | Ireturn | Iend | Iexit _ -> false
-  | Iop (Ialloc _)
-  | Iop (Ipoll _)
-  | Iraise _ -> true  (* Iraise included here because it contains a poll *)
-  | Iop _ -> path_polls f.next
+module IntSet = Set.Make (Int)
 
- (* Check a sequence of instructions from [f] and return whether the
-    function prologue requires a poll, by virtue of the instructions not
-    causing a poll already. *)
-let requires_prologue_poll ~future_funcnames (f : Mach.instruction) : bool =
-  let rec check_path i =
-  match i.desc with
-  | Iifthenelse (_, i0, i1) ->
-      (check_path i0) || (check_path i1) || (check_path i.next)
-  | Iswitch (_, acts) ->
-      (Array.exists check_path acts) || (check_path i.next)
+(* Approximation of the behavior of a block of code. *)
+type approx =
+  | PRTC             (** The block can do a Potentially-Recursive Tail-Call
+                         without allocating *)
+  | Return           (** The block can return or (non-rec) tail-call
+                         without allocating *)
+  | Exit of IntSet.t (** The block can reach one of these exits
+                         without allocating; 0 represents Iend *)
+  | Alloc            (** The block allocates in all cases *)
+
+(* Upper bound of two approximations *)
+let join a1 a2 =
+  match a1, a2 with
+  | PRTC, _ | _, PRTC -> PRTC
+  | Return, _ | _, Return -> Return
+  | Exit s1, Exit s2 -> Exit (IntSet.union s1 s2)
+  | (Exit _ as a), Alloc | Alloc, (Exit _ as a) -> a
+  | Alloc, Alloc -> Alloc
+
+(* Check a sequence of instructions from [f] and return
+   an approximation of its behavior *)
+let rec path_approx ~fwd_funcs f =
+  match f.desc with
+  | Iifthenelse (_, i0, i1) -> branch_approx ~fwd_funcs [| i0; i1 |] f.next
+  | Iswitch (_, acts) -> branch_approx ~fwd_funcs acts f.next
   | Icatch (_, handlers, body) ->
-     (check_path body)
-     ||
-      (List.exists check_path (List.map (fun (_, handler_code) ->
-        handler_code) handlers)) || (check_path i.next)
-  | Itrywith (body, handler) ->
-      (check_path body) || (check_path handler) || (check_path i.next)
-  | Iop (Itailcall_ind) -> true
-  | Iop (Itailcall_imm { func; _ }) ->
-    if (String.Set.mem func future_funcnames) then
-      (* this means we have a call to a function that might be a self call
-         or a call to a future function (which won't have a poll) *)
-      true
-    else
-      check_path i.next
-  | Iop (Ialloc _)
-  | Iop (Ipoll _)
-  | Iraise _ -> false  (* Iraise included here because it contains a poll *)
-  | Iend | Ireturn | Iexit _ -> false
-  | Iop _ -> check_path i.next
-  in check_path f
+    begin match path_approx ~fwd_funcs body with
+    | PRTC -> PRTC
+    | (Exit _ | Return) as a ->
+      let add acc (_, h) = join acc (path_approx ~fwd_funcs h) in
+      let a1 = List.fold_left add a handlers in
+      let a2 =
+        match a1 with
+        | PRTC -> PRTC
+        | Return -> Return
+        | Exit s1 ->
+          let remove s (i, _) = IntSet.remove i s in
+          Exit (List.fold_left remove s1 handlers)
+        | Alloc -> assert false
+      in
+      seq_approx ~fwd_funcs a2 f.next
+    | Alloc -> Alloc
+    end
+  | Itrywith (body, _handler) ->
+    (* in general, this would be:
+         seq_approx ~fwd_funcs
+                    (join (path_approx ~fwd_funcs body)
+                          (path_approx ~fwd_funcs handler))
+                    f.next
+       but we insert a poll at every raise, so we can ignore the handler
+       as if it allocated immediately *)
+      seq_approx ~fwd_funcs (path_approx ~fwd_funcs body) f.next
+  | Ireturn -> Return
+  | Iop (Itailcall_ind) -> PRTC
+  | Iop (Itailcall_imm {func; _}) ->
+    if String.Set.mem func fwd_funcs then PRTC else Return
+  | Iend -> Exit (IntSet.singleton 0)
+  | Iexit i -> Exit (IntSet.singleton i)
+  | Iraise _ -> Alloc (* Iraise included here because it has a poll inserted *)
+  | Iop (Ialloc _ | Ipoll _) -> Alloc
+  | Iop _ -> path_approx ~fwd_funcs f.next
+
+and branch_approx ~fwd_funcs branches next =
+  assert (branches <> [| |]);
+  let join_branch acc branch = join acc (path_approx ~fwd_funcs branch) in
+  seq_approx ~fwd_funcs (Array.fold_left join_branch Alloc branches) next
+
+and seq_approx ~fwd_funcs a next =
+  match a with
+  | Alloc -> Alloc
+  | Exit s when IntSet.mem 0 s ->
+    join (Exit (IntSet.remove 0 s)) (path_approx ~fwd_funcs next)
+  | Exit _ -> a
+  | PRTC -> PRTC
+  | Return -> join Return (path_approx ~fwd_funcs next)
+
+let path_polls f =
+  match path_approx ~fwd_funcs:String.Set.empty f with
+  | Alloc -> true
+  | _ -> false
+
+let requires_prologue_poll ~future_funcnames f =
+  match path_approx ~fwd_funcs:future_funcnames f with
+  | PRTC -> true
+  | _ -> false
 
 (* This determines whether from a given instruction we unconditionally
    allocate and this is used to avoid adding polls unnecessarily *)
