bf0054980c33cb896a73b329fbc179265c8a18f0
Antonin Décimo <antonin@tarides.com>
2023-06-17 01:26:39 +0200

    ocamltest: fix recursive expansion of variables
    ocamltest uses Buffer.add_substitute to perform variable substitution.
    The function also replaces escaped `$` characters (i.e., the string
    `\$`) with the character `$`.
    ocamltest would apply substitution in a loop until the input and
    substituted strings were equal; however, removing the escape character
    during the first iteration would create variables during the second
    iteration.
    Consider now that a fixpoint is reached when no substitutions have
    been performed.
    
    Other problems occur when substituting on the whole string: the number
    of substitutions on the whole string is determined by the maximum
    number of recursive expansions of a single variable, should it expand
    in turn to another variable. With this algorithm, a variable that
    should be expanded only once would be expanded at most as many times
    as the variable with the maximum number of expansions of the original
    string, thus exhibiting inconsistent behavior.
    
    The solution is to apply substitution recursively only on the expanded
    values, not always on the whole string, and to keep track as above if
    substitutions have been performed.

--- ocamltest/environments.ml
+++ ocamltest/environments.ml
@@ -29,21 +29,27 @@ let to_bindings env =
   in
   VariableMap.fold f env []
 
-let expand_aux env value =
-  let bindings = to_bindings env in
-  let f (variable, value) = ((Variables.name_of_variable variable), value) in
-  let simple_bindings = List.map f bindings in
-  let subst s = try (List.assoc s simple_bindings) with Not_found -> "" in
-  let b = Buffer.create 100 in
-  try Buffer.add_substitute b subst value; Buffer.contents b with _ -> value
-
-let rec expand env value =
-  let expanded = expand_aux env value in
-  if expanded=value then value else expand env expanded
+let rec expand simple_bindings value =
+  let fixpoint = ref true in
+  let subst s =
+    try
+      let v = List.assoc s simple_bindings in
+      fixpoint := false;
+      let expanded, fixpoint_expanded = expand simple_bindings v in
+      if fixpoint_expanded then v else expanded
+    with Not_found -> "" in
+  let b = Buffer.create (String.length value) in
+  try Buffer.add_substitute b subst value; Buffer.contents b, !fixpoint
+  with _ -> value, true
 
 let expand env = function
   | None -> raise Not_found
-  | Some value -> expand env value
+  | Some value ->
+     let bindings = to_bindings env in
+     let f (variable, value) = ((Variables.name_of_variable variable), value) in
+     let simple_bindings = List.map f bindings in
+     let expanded, _ = expand simple_bindings value in
+     expanded
 
 let append_to_system_env environment env =
   (* Augment env with any bindings which are only in environment. This must be
